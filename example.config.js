d4e89dcc41be5c7ef20728c51ccace4727ec9ad5
INEX07 <46562212+INEX07@users.noreply.github.com>
2020-03-18 20:04:22 +0545

    Create example.config.js

---
+++ example.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mdb: `mongodb://localhost/blogs`,
+    port: 3000,
+    session: "OFIHGIOUFHBWPIGBQIOWEHGI[EWNGIOEWNQFIPWNFIPOWNEMIGOPWNEGPIOEWNHIKhfoiawehngiphngipernhiperhiopw41564184132132356785!^%$#&$*&^)&%%^$%@#$^#@^%$&$!@!@%^*()_)(+=-",
+    admins: []
+};
